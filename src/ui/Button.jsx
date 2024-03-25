1a3b830157aa6779e98074561c15651a9004fe0a
rstulug <rstulug@gmail.com>
2024-03-25 21:55:02 +0300

    404 error solved

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,7 +8,7 @@ function Button({ btnName, type, onClick, disabled, size, to }) {
     alternative:
       "text-gray-900  bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700   dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 mx-2 my-2 ",
 
-    dark: "text-white bg-gray-800 hover:bg-gray-900   rounded-lg  dark:bg-gray-800 dark:hover:bg-gray-700 dark:border-gray-700 mx-2 my-2 ",
+    dark: "text-white bg-gray-800 hover:bg-gray-900  rounded-lg  dark:bg-gray-800 dark:hover:bg-gray-700 dark:border-gray-700 mx-2 my-2 ",
 
     light:
       "text-gray-900 bg-white border border-gray-300  hover:bg-gray-100  rounded-lg  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600  mx-2 my-2 ",
