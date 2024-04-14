37413184009c4f5993564a017a340defb10695c3
rstulug <rstulug@gmail.com>
2024-04-14 21:34:17 +0300

    Spinner centered

--- src/ui/Spinner.jsx
+++ src/ui/Spinner.jsx
@@ -1,6 +1,8 @@
 function Spinner() {
   return (
-    <div className="my-auto flex h-20 w-20 animate-spin rounded-full border-8 border-gray-300 border-t-blue-600 " />
+    <div className="flex h-96 w-full items-center justify-center">
+      <div className="my-auto flex h-16 w-16 animate-spin rounded-full border-8 border-gray-300 border-t-blue-600 " />
+    </div>
   );
 }
 
